57e0db22dbacd4f50404e676137ce72413177e09
HJ17J <hyeonj0130@gmail.com>
2024-06-03 19:52:16 +0900

    CHORE : [BE] 초기 설정

---
+++ src/main/java/ramyunlab_be/RamyunLabBeApplication.java
@@ -0,0 +1,13 @@
+package ramyunlab_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamyunLabBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamyunLabBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ramyunlab_be/RamyunLabBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ramyunlab_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamyunLabBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
