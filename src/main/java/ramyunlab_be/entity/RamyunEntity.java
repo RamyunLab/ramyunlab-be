446aeac0bbc1823fdc3da5eb7b77cb8ae7b65ee7
CHUSUEYEON <rmffkfk226@naver.com>
2024-06-06 13:46:10 +0900

    feat/entity 1차

--- src/main/java/ramyunlab_be/entity/RamyunEntity.java
+++ src/main/java/ramyunlab_be/entity/RamyunEntity.java
@@ -1,7 +1,7 @@
 package ramyunlab_be.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,14 +14,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "ramyun")
 public class RamyunEntity {
-    private long idx;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="r_idx", updatable = false)
+    private long ramyunIdx;
+
+    @Column(name = "r_name", nullable = false, length = 20)
     private String ramyunName;
+
+    @Column(name = "r_img", nullable = true, length = 255)
     private String ramyunImg;
+
+    @Column(name = "r_kcal", nullable = false)
     private Integer ramyunKcal;
+
+    @Column(name = "r_noodle", nullable = false)
     private Boolean noodle;
+
+    @Column(name = "r_isCup", nullable = false)
     private Boolean isCup;
+
+    @Column(name = "r_cooking", nullable = false)
     private Boolean cooking;
+
+    @Column(name = "r_gram", nullable = false)
     private Integer gram;
+
+    @Column(name = "r_Na", nullable = false)
     private Integer ramyunNa;
+
+    @Column(name = "r_scoville", nullable = true)
     private Integer scoville;
+
+    @ManyToOne
+    @JoinColumn(name = "brand", nullable = false)
+    @JsonBackReference
+    private BrandEntity brand;
 }
