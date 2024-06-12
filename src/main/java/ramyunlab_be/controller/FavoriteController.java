e74bda90a2292a60f3fe73ee547784d0986d7a00
이대원 <1ee_DW@naver.com>
2024-06-12 14:59:05 +0900

    feat/favorite 컨트롤러 예외처리 추가

--- src/main/java/ramyunlab_be/controller/FavoriteController.java
+++ src/main/java/ramyunlab_be/controller/FavoriteController.java
@@ -6,15 +6,13 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ramyunlab_be.dto.FavoriteDTO;
 import ramyunlab_be.dto.ResDTO;
 import ramyunlab_be.entity.FavoriteEntity;
@@ -53,5 +51,12 @@ public class FavoriteController {
                 .message("찜목록 호출 완료")
                 .data(favList)
                 .build());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ResDTO> handleValidationException(ValidationException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(ResDTO.builder().statusCode(StatusCode.BAD_REQUEST).message(e.getMessage()).build());
     }
 }
