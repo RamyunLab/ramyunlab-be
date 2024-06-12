2c102ef36e446bfbff860c264624075e0b69e325
HJ17J <hyeonj0130@gmail.com>
2024-06-12 15:46:39 +0900

    fix: [BE] 게임 랜덤 조회 쿼리 수정

--- src/main/java/ramyunlab_be/repository/GameRepository.java
+++ src/main/java/ramyunlab_be/repository/GameRepository.java
@@ -9,14 +9,14 @@ import ramyunlab_be.entity.RamyunEntity;
 public interface GameRepository extends JpaRepository<RamyunEntity, Long> {
 
   // round 수를 받아 해당 수만큼 라면 데이터 랜덤 조회
-  @Query(value = "SELECT * FROM Ramyun ORDER BY RAND() LIMIT :round", nativeQuery = true)
+  @Query(value = "SELECT * FROM ramyun ORDER BY RAND() LIMIT :round", nativeQuery = true)
   List<RamyunEntity> findRandomListByRound (@Param("round") int round);
 
-  @Query(value = "SELECT R.* FROM ( SELECT RAMYUN.*, "
+  @Query(value = "SELECT r.* FROM ( SELECT ramyun.*, "
                  + "ROW_NUMBER() OVER (PARTITION BY r_scoville ORDER BY r_scoville) AS rnk "
-                 + "FROM RAMYUN) R "
+                 + "FROM ramyun) r "
                  + "WHERE rnk = 1 AND r_scoville IS NOT NULL "
-                 + "ORDER BY RAND()"
+                 + "ORDER BY RAND() "
                  + "LIMIT 11", nativeQuery = true)
   List<RamyunEntity> findRandomListByScoville();
 
