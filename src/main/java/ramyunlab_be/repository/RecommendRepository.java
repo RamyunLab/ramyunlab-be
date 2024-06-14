ca4502d90bf10d093d2f74959731e62584b1b706
이대원 <1ee_DW@naver.com>
2024-06-14 10:58:51 +0900

    feat: 내가 공감한 리뷰 목록 호출 -be

--- src/main/java/ramyunlab_be/repository/RecommendRepository.java
+++ src/main/java/ramyunlab_be/repository/RecommendRepository.java
@@ -1,10 +1,16 @@
 package ramyunlab_be.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ramyunlab_be.entity.FavoriteEntity;
 import ramyunlab_be.entity.RecommendEntity;
 import ramyunlab_be.entity.ReviewEntity;
 
 @Repository
 public interface RecommendRepository extends JpaRepository<RecommendEntity, Long> {
+    // @Query("SELECT r FROM RecommendEntity r WHERE r.userIdx = :userIdx");
+    Page<RecommendEntity> findByUser_UserIdx(Pageable pageable, Long userIdx);
+
 }
