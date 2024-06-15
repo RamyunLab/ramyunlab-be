db70ece389bfdf29f2445fe719fce9faffc8d867
HJ17J <hyeonj0130@gmail.com>
2024-06-15 20:39:14 +0900

    feat: [BE] 게임 결과 라면 조회

--- src/main/java/ramyunlab_be/service/GameService.java
+++ src/main/java/ramyunlab_be/service/GameService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ramyunlab_be.dto.GameDTO;
+import ramyunlab_be.dto.RamyunDTO;
 import ramyunlab_be.entity.RamyunEntity;
 import ramyunlab_be.repository.GameRepository;
 
@@ -20,34 +21,34 @@ public class GameService {
     log.info("service round: {}", round);
     List<RamyunEntity> result = gameRepository.findRandomListByRound(round);
     List<GameDTO> worldCupList = new ArrayList<>();
-    log.info("result: {}", result);
 
     for (RamyunEntity ramyun : result) {
-      worldCupList.add(GameDTO.builder()
-                                  .r_idx(ramyun.getRamyunIdx())
-                                  .r_img(ramyun.getRamyunImg())
-                                  .r_name(ramyun.getRamyunName())
-                                  .build());
+      worldCupList.add(convertToDTO(ramyun));
     }
     return worldCupList;
   }
 
-
   public List<GameDTO> getRandomListByScoville(){
     List<RamyunEntity> result = gameRepository.findRandomListByScoville();
     List<GameDTO> scovilleList = new ArrayList<>();
-    log.info("result: {}", result);
-
     for (RamyunEntity ramyun : result) {
-      scovilleList.add(GameDTO.builder()
-                              .r_idx(ramyun.getRamyunIdx())
-                              .r_img(ramyun.getRamyunImg())
-                              .r_name(ramyun.getRamyunName())
-                              .r_scoville(ramyun.getScoville())
-                              .build());
+      scovilleList.add(convertToDTO(ramyun));
     }
     return scovilleList;
   }
 
+  public GameDTO getResult (Long ramyunIdx){
+    RamyunEntity result = gameRepository.findById(ramyunIdx)
+                                        .orElseThrow(() -> new RuntimeException("해당 라면이 존재하지 않습니다."));
+    return convertToDTO(result);
+  }
 
+  public GameDTO convertToDTO(RamyunEntity entity){
+    return GameDTO.builder()
+                         .r_idx(entity.getRamyunIdx())
+                         .r_img(entity.getRamyunImg())
+                         .r_name(entity.getRamyunName())
+                         .r_scoville(entity.getScoville())
+                         .build();
+  }
 }
