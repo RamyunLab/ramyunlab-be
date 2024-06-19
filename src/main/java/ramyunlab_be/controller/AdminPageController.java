7ad40d8e0b47a888f56b0877cb5ab2ddaaea5159
CHUSUEYEON <rmffkfk226@naver.com>
2024-06-20 00:15:02 +0900

    fix : [BE] review 신고 상태 변경 오류 수정

--- src/main/java/ramyunlab_be/controller/AdminPageController.java
+++ src/main/java/ramyunlab_be/controller/AdminPageController.java
@@ -1,10 +1,12 @@
 package ramyunlab_be.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class AdminPageController {
     @GetMapping("/login")
@@ -15,6 +17,7 @@ public class AdminPageController {
     @GetMapping("/userPage")
     public String adminUserPage(Model model,
                                 @AuthenticationPrincipal String userIdx) {
+        log.warn("UserIDx {}", userIdx);
         return "adminUser";
     }
 
